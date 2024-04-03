802e4862395d86f152a136680a0108491851b97a
Priyanka Karpe <146506142+Priyankaykarpe@users.noreply.github.com>
2024-04-03 11:20:21 +0530

    Ak--code

--- Git/src/com/Demo.java
+++ Git/src/com/Demo.java
@@ -8,6 +8,19 @@ public class Demo
 	{
 		System.out.println("This is M1");
 	}
+
+	public void m1()
+	{
+		System.out.println("This is M1");
+	}public void m2()
+	{
+		System.out.println("This is M2");
+	}
+
+	public void m3()
+	{
+		System.out.println("This is M3");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("This is Git Demo");
